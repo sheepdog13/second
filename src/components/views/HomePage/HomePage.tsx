283b231e0898db802d18b865bbe5d555f4346ae4
sheepdog13 <mikecho1234@naver.com>
2024-01-14 23:57:40 +0900

    style : HomePage css

--- src/components/views/HomePage/HomePage.tsx
+++ src/components/views/HomePage/HomePage.tsx
@@ -6,11 +6,32 @@ const Wapper = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   background-color: #000;
 `;
 
+const Logo = styled.img`
+  width: 150px;
+  height: 51px;
+`;
+
+const Btn = styled.button`
+  width: 172px;
+  height: 88px;
+  border-radius: 20px;
+  background-color: #fff;
+  font-size: 25px;
+  margin-top: 41px;
+`;
+
 function HomePage() {
-  return <Wapper>home</Wapper>;
+  return (
+    <Wapper>
+      <Logo src={`${process.env.PUBLIC_URL}/img/logo-black.png`} alt="logo" />
+      <Btn>주문하러 가기</Btn>
+    </Wapper>
+  );
 }
 
 export default HomePage;
